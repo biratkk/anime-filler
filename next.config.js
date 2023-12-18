39c6f6c9b96fd9999e77f60db3c8ff0088f708d1
Birat Koirala <koiralabirat@gmail.com>
2023-12-18 16:41:46 +0000

    Added cors policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  headers: async () => [
+    {
+      source: "/api/*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,DELETE,PATCH,POST,PUT",
+        },
+      ],
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
